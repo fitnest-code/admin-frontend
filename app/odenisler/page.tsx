af0c3cc9afcea40420b8ccc5f347179db4ea28de
Ramin7DEV <raminbakhshaliyev@gmail.com>
2026-05-17 16:22:56 +0400

    Implement payments analytics UI and shared date range

---
+++ app/odenisler/page.tsx
@@ -0,0 +1,10 @@
+import { AdminLayout } from '@/components/layout/admin-layout'
+import { PaymentsAnalyticsPage } from '@/components/payments/payments-analytics-page'
+
+export default function OdenislerPage() {
+  return (
+    <AdminLayout>
+      <PaymentsAnalyticsPage />
+    </AdminLayout>
+  )
+}
